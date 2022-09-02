af03983898046743c0661bef0f69d8265244e917
Andrew Cuga <andrewcuga@b0456.attlocal.net>
2022-09-02 15:24:48 -0400

    Checkpoint

---
+++ src/runMe.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/sha512"
+	"encoding/base64"
+	"fmt"
+	"image"
+	"image/color/palette"
+	"image/png"
+	"os"
+)
+
+func main() {
+	// make a hash of the personal info
+	userHash := getHash()
+	fmt.Println("The hashed value is: ", userHash)
+
+	// bw := []color.Color{color.Black, color.White}
+	// fmt.Println(bw)
+
+	outputImage := image.NewPaletted(
+		image.Rect(0, 0, 100, 200),
+		palette.WebSafe,
+	)
+	writeImage(outputImage)
+	//outputPath := time.Now().Format("01-02-2006 15:04:05") + ".png"
+
+	// TODO:
+	// make an image of the hash of the IP address
+	// save to file?
+}
+func writeImage(outputImage *image.Paletted) {
+
+	outputPath := "identicon.png"
+	out, err := os.Create(outputPath)
+	if err != nil {
+		fmt.Println("Could not create image")
+		return
+	}
+	png.Encode(out, outputImage)
+	out.Close()
+	fmt.Println("identicon written to ", outputPath)
+}
+func getHash() string {
+	// get a string (eg IP address, email)
+	fmt.Println("Enter Your Personal Information: ")
+	var personalInfo string
+	fmt.Scanln(&personalInfo)
+
+	hasher := sha512.New()
+	bv := []byte(personalInfo)
+	hasher.Write(bv)
+
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	return sha
+}
+
+/**
+import (
+    "image"
+)
+func main() {
+    myImg := image.NewRGBA(image.Rect(0, 0, 12, 6))
+        out, err := os.Create("cat.png")
+        png.Encode(out, myImg)
+        out.Close()
+}
+*/
